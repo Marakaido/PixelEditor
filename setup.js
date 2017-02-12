72a9f65020e21fa8eec3c3150409eb9879747882
Marakaido <yaroslav.mykoliv28@gmail.com>
2017-02-12 12:17:40 +0200

    Add google authentication

--- setup.js
+++ setup.js
@@ -60,6 +60,12 @@ var Application = {
         this.initContext();
         this.calculateZoom();
         GUI.init();
+        this.initFirebase();
+
+        this.userPic = document.getElementById('user-pic');
+        this.userName = document.getElementById('user-name');
+        this.signInButton = document.getElementById('sign-in');
+        this.signOutButton = document.getElementById('sign-out');
     },
 
     initCanvas: function(width, height)
@@ -173,6 +179,74 @@ var Application = {
                 this.data[redPos+3] = color.alpha;
             }
         };
+    },
+
+    initFirebase: function() {
+        // Shortcuts to Firebase SDK features.
+        this.auth = firebase.auth();
+        this.database = firebase.database();
+        this.storage = firebase.storage();
+        // Initiates Firebase auth and listen to auth state changes.
+        this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+    },
+
+    onAuthStateChanged: function(user) {
+        if (user) 
+        { // User is signed in!
+            // Get profile pic and user's name from the Firebase user object.
+            var profilePicUrl = user.photoURL; // Only change these two lines!
+            var userName = user.displayName; 
+
+            // Set the user's profile pic and name.
+            this.userPic.style.backgroundImage = 'url(' + profilePicUrl + ')';
+            this.userName.textContent = userName;
+
+            // Show user's profile and sign-out button.
+            this.userName.removeAttribute('hidden');
+            this.userPic.removeAttribute('hidden');
+            this.signOutButton.removeAttribute('hidden');
+
+            // Hide sign-in button.
+            this.signInButton.setAttribute('hidden', 'true');
+
+            // We load currently existing chant messages.
+            this.loadMessages();
+        } 
+        else 
+        { // User is signed out!
+            // Hide user's profile and sign-out button.
+            this.userName.setAttribute('hidden', 'true');
+            this.userPic.setAttribute('hidden', 'true');
+            this.signOutButton.setAttribute('hidden', 'true');
+
+            // Show sign-in button.
+            this.signInButton.removeAttribute('hidden');
+        }
+    },
+
+    signIn: function() {
+        // Sign in Firebase using popup auth and Google as the identity provider.
+        var provider = new firebase.auth.GoogleAuthProvider();
+        this.auth.signInWithPopup(provider);
+    },
+
+    signOut: function() {
+        // Sign out of Firebase.
+        this.auth.signOut();
+    },
+
+    checkSignedInWithMessage: function() {
+        // Return true if the user is signed in Firebase
+        if (this.auth.currentUser) {
+            return true;
+        }
+        // Display a message to the user using a Toast.
+        var data = {
+            message: 'You must sign-in first',
+            timeout: 2000
+        };
+        this.signInSnackbar.MaterialSnackbar.showSnackbar(data);
+        return false;
     }
 };
 
